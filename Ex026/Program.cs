adf473871e7c5fe4891b74e0101a38a29d6d9855
Elena <velenavl@inbox.ru>
2022-09-11 18:28:40 +0300

    Ex 026

---
+++ Ex026/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число и выдает количество цифр в нем.
+Console.WriteLine("Введите число N: ");
+bool isNumber = int.TryParse(Console.ReadLine(), out int N);
+
+if(!isNumber)
+{
+    Console.WriteLine("Не число");
+    return;
+}
+
+int GetLenNumber(int number)
+{
+    int count = 0;
+    if(number == 0)
+    {
+        return 1;
+    }
+    while(number != 0)
+    {
+        count++;
+        number = number / 10;
+    }
+    
+    return count;
+}
+int result = GetLenNumber(N);
+Console.WriteLine(result);
