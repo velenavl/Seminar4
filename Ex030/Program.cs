01ebcd4bdd025b02087f0842b5521317648fc5f3
Elena <velenavl@inbox.ru>
2022-09-11 19:32:01 +0300

    Ex 030

---
+++ Ex030/Program.cs
@@ -0,0 +1,52 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// void PrintSqares(int[] sqares)
+// {
+//     int i = 0;
+//     int Len = sqares.Length;
+//     while (i < Len)
+//     {
+//         Console.WriteLine(sqares[i]);
+//         i++;
+//     }
+// }
+
+// int[] GenrArrey(int N)
+// {
+//     int[] arr = new int[N];
+//     Random random = new Random();
+//     int number = random.Next(0, 2);
+//     for (int i = 0; i < N; i++)
+//     {
+//        arr[i] = random.Next(0, 2); 
+//     }
+//     return arr;
+// }
+
+// PrintSqares(GenrArrey(8));
+
+int[] FillArray(int n)
+{
+    int[] newArray = new int [n];
+    for (int i=0;i<n; i++)
+    {
+    newArray[i]=new Random().Next(0, 2);    
+    }
+    return newArray;
+}
+
+void PrintArr(int[] array)
+{
+    for (int i=0; i<array.Length;i++)   
+    {
+    Console.Write($"{array[i]}");    
+    }
+}
+
+Console.WriteLine("Введите размер массива number");
+bool isNumber = int.TryParse(Console.ReadLine(), out int number);
+if (isNumber==false || number<1)
+{
+    Console.WriteLine("Неверный ввод");
+    return;
+}
+PrintArr(FillArray(number));
