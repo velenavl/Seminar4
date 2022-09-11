7ece96442a61e4d26eaba35fbc4d5604aac7fd35
Elena <velenavl@inbox.ru>
2022-09-12 00:37:22 +0300

    HW 027

---
+++ HW027/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+Console.WriteLine("Введите число: ");
+bool isNumber = int.TryParse(Console.ReadLine(), out int n);
+
+if (!isNumber)
+{
+    Console.WriteLine("Не число");
+    return;
+}
+
+int GetSumNumber(int number)
+{
+    int counter = Convert.ToString(number).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+      advance = number - number % 10;
+      result = result + (number - advance);
+      number = number / 10;
+    }
+    return result;
+}
+
+Console.WriteLine(GetSumNumber(n));
